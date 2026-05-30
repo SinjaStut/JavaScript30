5c3fc2615a8789c9b3d22a417ad12653477eca3d
Sinja Stutterecker <sinja.stutterecker@icloud.com>
2026-05-30 23:09:50 +0200

    Add test for curly finding

---
+++ spec/fixedSpec.js
@@ -0,0 +1,8 @@
+describe('1. curly – removeTransition', function () {
+  it('should return false if propertyName is not transform', function () {
+    expect(removeTransition('opacity')).toEqual(false);
+  });
+  it('should return true if propertyName is transform', function () {
+    expect(removeTransition('transform')).toEqual(true);
+  });
+});
