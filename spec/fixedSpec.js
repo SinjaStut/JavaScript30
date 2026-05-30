f7f4c0ae07efb912fb0a30dd68ecfd99a656f98c
Sinja Stutterecker <sinja.stutterecker@icloud.com>
2026-05-30 23:22:33 +0200

    Add test for no-var finding

--- spec/fixedSpec.js
+++ spec/fixedSpec.js
@@ -6,3 +6,10 @@ describe('1. curly – removeTransition', function () {
     expect(removeTransition('transform')).toEqual(true);
   });
 });
+
+describe('2. no-var – debounce', function () {
+  it('should return a function', function () {
+    const result = debounce(function() {});
+    expect(typeof result).toEqual('function');
+  });
+});
